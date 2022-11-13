c6515a4fcd36c514984d59f9cf71d9192114309c
DESKTOP-E6U9IT1\t2sun <t2suna@gmail.com>
2022-11-13 12:01:24 +0900

    Go07

--- model/todo.go
+++ model/todo.go
@@ -13,9 +13,18 @@ type (
 	}
 
 	// A CreateTODORequest expresses ...
-	CreateTODORequest struct{}
+	CreateTODORequest struct {
+		Subject     string `json:subject`
+		Description string `json:description`
+	}
 	// A CreateTODOResponse expresses ...
-	CreateTODOResponse struct{}
+	CreateTODOResponse struct {
+		ID          int       `json:id`
+		Subject     string    `json:subject`
+		Description string    `json:description`
+		CreatedAt   time.Time `json:created_at`
+		UpdatedAt   time.Time `json:updated_at`
+	}
 
 	// A ReadTODORequest expresses ...
 	ReadTODORequest struct{}
