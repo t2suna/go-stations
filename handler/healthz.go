b5cfd68f360595cfafd618fcc8c3f5cba55ecc67
DESKTOP-E6U9IT1\t2sun <t2suna@gmail.com>
2022-11-13 10:12:07 +0900

    Go 05

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -16,6 +18,10 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{Message: "OK"}
+	tmpHR := &model.HealthzResponse{Message: "OK"}
+	err := json.NewEncoder(w).Encode(tmpHR.Message)
+	if err != nil {
+		log.Println(err)
+	}
 
 }
